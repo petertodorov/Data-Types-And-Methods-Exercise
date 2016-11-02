9140670703a64b3cec13c8c158961111fc3975e3
Peter Todorov <petodorov@gmail.com>
2016-11-02 12:41:14 +0200

    ExerciseFive
    CommitedJudge - works

---
+++ Exercise5BooleanVariable/Exercise5BooleanVariable/Exercise5BooleanVariable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Exercise5BooleanVariable
+{
+    static void Main(string[] args)
+    {
+        string state = Console.ReadLine();
+        bool converted = Convert.ToBoolean(state);
+        if (converted)
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
+
