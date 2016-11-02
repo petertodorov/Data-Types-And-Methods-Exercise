66e20bf121eb34913902bd5298a45d57abfbdb95
Peter Todorov <petodorov@gmail.com>
2016-11-02 14:59:06 +0200

    ExerciseNine
    CommitedJudge-works.

---
+++ DataTypeAndMethodsExercise/Exercise9ReverseCharacters/Exercise9ReverseCharacters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Exercise9ReverseCharacters
+{
+    static void Main(string[] args)
+    {
+        string[] toReverse = new string[3];        
+        for (int j = 0; j < toReverse.Length; j++)
+        {
+            toReverse[j] = Console.ReadLine();
+        }
+        Array.Reverse(toReverse);
+        for (int i = 0; i < toReverse.Length; i++)
+        {
+            Console.Write(toReverse[i]);
+        }
+    }
+}
