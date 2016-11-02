d7db3b8d44946abcffbd1c7ab2b8eab6f1d55e0b
Peter Todorov <petodorov@gmail.com>
2016-11-02 12:50:45 +0200

    ExerciseSix
    CommitedJudge-works

---
+++ DataTypeAndMethodsExercise/Exercise6StringsAndObjects/Exercise6StringsAndObjects.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Exercise6StringsAndObjects
+{
+    static void Main(string[] args)
+    {
+        string firstWord = "Hello";
+        string secondWord= "World";
+        object sentence = firstWord + " " + secondWord;
+        string casted = (string)sentence;
+        Console.WriteLine(casted);
+
+    }
+}
+
