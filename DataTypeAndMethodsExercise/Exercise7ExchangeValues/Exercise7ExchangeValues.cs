7da62ea69f9c18ce4b5a4d2bb5c2984bf13789d2
Peter Todorov <petodorov@gmail.com>
2016-11-02 13:09:38 +0200

    ExerciseSeven
    CommitedJudge-works

---
+++ DataTypeAndMethodsExercise/Exercise7ExchangeValues/Exercise7ExchangeValues.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Exercise7ExchangeValues
+{
+    static void Main(string[] args)
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Before:\na = {a}\nb = {b}");
+        int temp = a;
+        a = b;
+        b = temp;
+        Console.WriteLine($"After:\na = {a}\nb = {b}");
+
+
+    }
+}
+
